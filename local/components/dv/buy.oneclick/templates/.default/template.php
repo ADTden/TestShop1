2f099620183f066576e47c25688543cd1b778e74
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-18 15:43:12 +0300

    1

---
+++ local/components/dv/buy.oneclick/templates/.default/template.php
@@ -0,0 +1,12 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+ 
+?>
+<button class="prew_but">КУПИТЬ В 1 КЛИК</button>
+<div class="one-click-button" style="display:none;">
+	<input class="phone"></input>
+	<button class="btn-one-click" data-id-element="<?=$arParams["ID_ELEMENT"]?>" data-cur-path="<?=$APPLICATION->GetCurDir()?>">Оформить заказ</button>
+
+</div>
+<div class="status"></div>
+
+
